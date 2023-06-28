f15526fed6872f3a7d6b93a9e7b8a76b82a0be16
Maksl0217 <makslapkin00@gmail.com>
2023-06-28 22:58:36 +0300

    types_db.ts

---
+++ components/Box.tsx
@@ -0,0 +1,19 @@
+import { FC, ReactNode } from "react"
+import { twMerge } from "tailwind-merge"
+
+interface IBox {
+  children: ReactNode
+  className?: string
+}
+
+const Box: FC<IBox> = ({ children, className }) => {
+  return (
+    <div
+      className={twMerge(` bg-neutral-900 rounded-lg h-fit w-full`, className)}
+    >
+      {children}
+    </div>
+  )
+}
+
+export default Box
